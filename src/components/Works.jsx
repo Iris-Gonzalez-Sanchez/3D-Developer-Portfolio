19c2637fc758298540a0c9a70ad83b32474f9513
Iris Gonzalez Sanchez <irisgon-san@Iriss-Air.nyc.rr.com>
2023-05-03 17:55:30 -0400

    commented out live demos sections until further development

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -48,7 +48,8 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link }
                 alt="github"
                 className='w-1/2 h-1/2 object-contain'
               />
-            </div> */} //USE FOR ADDING WALKTHROUGHS OF PROJECTS
+            </div> */} 
+            {/* ^ //USE FOR ADDING WALKTHROUGHS OF PROJECTS */}
           </div>
         </div>
 
@@ -83,7 +84,9 @@ const Works = () => {
           variant={fadeIn("", "", 0.1, 1)}
           className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
-          Following projects showcase my skills and experience through real-world examples of my work. Each project is briefly described with links to code repositories and live demos in it. It reflects my ability to solve complex problems, work with different technologies, and manage projects effectively.
+          Following projects showcase my skills and experience through real-world examples of my work. Each project is briefly described with links to code repositories 
+          {/* and live demos in it */}
+          . It reflects my ability to solve complex problems, work with different technologies, and manage projects effectively.
         </motion.p>
       </div>
 

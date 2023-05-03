371443231e36d39b4d254401a9d8ae1e820ef35e
Iris Gonzalez Sanchez <irisgon-san@Iriss-Air.nyc.rr.com>
2023-05-03 18:04:41 -0400

    Removed variant and fadeIn parameters as they do not function

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,14 +4,12 @@ import { motion } from 'framer-motion';
 
 import { styles } from '../styles';
 import { services } from '../constants';
-import { fadeIn, textVariant } from '../utils/motion';
 import { SectionWrapper } from '../hoc';
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
     <Tilt className="xs:w-[250px] w-full">
       <motion.div
-        variant={fadeIn("right", "spring", index * 0.5, 0.75)}
         className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
       >
         <div
@@ -35,13 +33,12 @@ const ServiceCard = ({ index, title, icon }) => {
 const About = () => {
   return (
     <>
-    <motion.div variant={textVariant()}>
+    <motion.div>
       <p className={styles.sectionSubText}>Introduction</p>
       <h2 className={styles.sectionHeadText}>Overview.</h2>
     </motion.div>
 
     <motion.p
-      variant={fadeIn("", "", 0.1, 1)}
       className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
     >
       I'm a skilled software developer with experience in TypeScript and Javascript, and expertise in frameworks like React, Node.js, and Three.js. I'm a quick learner and collaborate closely with clients to create efficient, scalable, and user-friendly solutions that solve real-world problems. Let's work together to bring your ideas to life!

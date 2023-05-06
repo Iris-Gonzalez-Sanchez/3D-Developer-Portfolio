d913cac352dab6d9ba469913be1acb9aaa179f40
Iris Gonzalez Sanchez <irisgon-san@Iriss-Air.lan>
2023-05-05 20:22:10 -0400

    fixed background, removed testimonials

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 import { styles } from '../styles';
 import { SectionWrapper } from '../hoc';
 import { fadeIn, textVariant } from '../utils/motion';
-import { testimonials } from '../constants';
+// import { testimonials } from '../constants';
 
 
 const FeedbackCard = ({ index, testimonial, name, designation, company, image }) => (
@@ -42,16 +42,16 @@ const FeedbackCard = ({ index, testimonial, name, designation, company, image })
 const Feedbacks = () => {
   return (
     <div className='mt-12 bg-black-100 rounded-[20px]'>
-      <div className={`${styles.padding}
-      bg-tertiary rounded-2xl min-h-[300px]`}>
-        <motion.div variant={textVariant()}>
-          <p className={styles.sectionSubText}
-          >What others say</p> 
-          <h2 className={styles.sectionHeadText}
-          >Testimonials.</h2>
-        </motion.div>
-      </div>
-      <div className={`${styles.paddingX} -mt-20 pb-14 flex flex-wrap gap-7`}>
+      {/* <div className={`${styles.padding}
+      bg-tertiary rounded-2xl min-h-[300px]`}> */}
+        {/* <motion.div variant={textVariant()}> */}
+          {/* <p className={styles.sectionSubText}
+          >What others say</p>  */}
+          {/* <h2 className={styles.sectionHeadText}
+          >Testimonials.</h2> */}
+        {/* </motion.div> */}
+      {/* </div> */}
+      {/* <div className={`${styles.paddingX} -mt-20 pb-14 flex flex-wrap gap-7`}>
         {testimonials.map((testimonial, index) => (
           <FeedbackCard
             key={testimonial.name}
@@ -59,7 +59,7 @@ const Feedbacks = () => {
             {...testimonial}
           />
         ))}
-      </div>
+      </div> */}
     </div>
   )
 }
